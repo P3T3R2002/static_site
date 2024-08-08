07dda91100d3122af3b62d3ebc9c33588fe32a51
P3T3R2002 <banyai.p0227@gmail.com>
2024-08-08 21:18:12 +0200

    split image/link

--- src/split_node.py
+++ src/split_node.py
@@ -1,4 +1,14 @@
+import re
 from textnode import *
+
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)", text)
+
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_node = []
@@ -10,7 +20,49 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_node.append(TextNode(text, current_type))
                 if current_type == text_type:
                     current_type = old_node.text_type
-                else:current_type = text_type
-
-            
+                else:current_type = text_type       
     return new_node
+
+
+def split_nodes_links(old_nodes):
+    split = []
+    for old_node in old_nodes:
+        links = []
+        links.append(extract_markdown_links(old_node.text))
+        if links == [[]]:
+            split.append(old_node)
+        for link in links:   
+            for lnk in link:
+                split.append(TextNode(old_node.text.split(f"[{lnk[0]}]({lnk[1]})")[0], old_node.text_type))
+                old_node.text = old_node.text.replace(split[-1].text, "")
+                split.append(TextNode(lnk[0], "link", lnk[1]))
+                old_node.text = old_node.text.replace(f"[{lnk[0]}]({lnk[1]})", "")
+    return split
+
+# images/links = list of lists of the extracted images/links
+# split = list of the splitted old_nodes
+# old_nodes = list of TextNodes
+
+def split_nodes_images(old_nodes):
+    split = []
+    for old_node in old_nodes:
+        images = []
+        images.append(extract_markdown_images(old_node.text))
+        if images == [[]]:
+            split.append(old_node)
+        for image in images:   
+            for img in image:
+                split.append(TextNode(old_node.text.split(f"![{img[0]}]({img[1]})")[0], old_node.text_type))
+                old_node.text = old_node.text.replace(split[-1].text, "")
+                split.append(TextNode(img[0], "image", img[1]))
+                old_node.text = old_node.text.replace(f"![{img[0]}]({img[1]})", "")
+    return split
+
+def text_to_textnodes(text):
+    split = [TextNode(text, "text")]
+    split = split_nodes_delimiter(split, "**", "bold")
+    split = split_nodes_delimiter(split, "*", "italic")
+    split = split_nodes_delimiter(split, "`", "code")
+    split = split_nodes_images(split)
+    split = split_nodes_links(split)
+    return split
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -2,42 +2,31 @@ import unittest
 
 from textnode import *
 from split_node import *
+from htmlnode import *
 
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        nodes = ([  TextNode("This is a test **bold**", "bold"),
-                    TextNode("This is a test *italic*", "italic"),
-                    TextNode("This is a test`code`", "code"),
-                    TextNode("This is a test link", "link", "https://www.boot.dev"),
-                    TextNode("This is a test image", "image", "https://www.boot.dev"),
-        ])
-
-        node_code = [
-                        TextNode("This is text with a `code block` word", "text"),
-                        TextNode("This is a `testcode`", "text")
-                    ]
-        code_nodes = split_nodes_delimiter(node_code, "`", "code")
-        for i in range(0, len(code_nodes)):
-            print("----", code_nodes[i].text_node_to_html_node().to_html())
-
-        node_bold = [
-                        TextNode("This is text with a **bold block** word", "text"),
-                        TextNode("This is a **testcode**", "text")
-                    ]
-        bold_nodes = split_nodes_delimiter(node_bold, "**", "bold")
-        for i in range(0, len(bold_nodes)):
-            print("----", bold_nodes[i].text_node_to_html_node().to_html())
-
-        node_italic = [
-                        TextNode("This is text with a *italic block* word", "text"),
-                        TextNode("This is a *testcode*", "text")
-                    ]
-        italic_nodes = split_nodes_delimiter(node_italic, "*", "italic")
-        for i in range(0, len(italic_nodes)):
-            print("----", italic_nodes[i].text_node_to_html_node().to_html())
-
-   
+        text_node1 = text_to_textnodes("This is **bold** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)")
+        for node1 in text_node1:
+            print(node1)
+        print("\n")
+        text_node1 = text_to_textnodes("This is *italic* with an **bold** word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)")
+        for node1 in text_node1:
+            print(node1)
+        print("\n")
+        text_node1 = text_to_textnodes("This is `code block` with an *italic* word and a **bold** and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)")
+        for node1 in text_node1:
+            print(node1)
+        print("\n")
+        text_node1 = text_to_textnodes("This is ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) with an *italic* word and a `code block` and an **bold** and a [link](https://boot.dev)")
+        for node1 in text_node1:
+            print(node1)
+        print("\n")
+        text_node1 = text_to_textnodes("This is *italic* with an [link](https://boot.dev) word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a **bold**")
+        for node1 in text_node1:
+            print(node1)
+        print("\n")
 
 if __name__ == "__main__":
     unittest.main()
